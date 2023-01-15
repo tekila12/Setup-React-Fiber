d68effa34ec3742256759f7ef20aeff1bcad67b7
Ajnur <radovic.ajnur96@gmail.com>
2023-01-15 20:33:47 +0100

    zoom in animation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ import {Setup} from './Setup'
 import { Suspense } from 'react';
 import { DirectionalLight } from 'three';
 function App() {
-  const { nodes, materials, animations } = useGLTF('/wholeSetup1.glb')
+
   return (
     <div style={{ width: "100vw", height: "100vh", background:'black' }}>   
     <Suspense fallback={null}>
     <Canvas camera={{ position: [-30, 40, 80], fov: 55, near: 1, far: 20000 }} >
     
     
-    <Setup receiveShadow scale ={2.21} position={[-1.3,-1.3,39.1]} rotation={[-0.22,12.45, 0.06]} animations={animations}/>
+    <Setup receiveShadow scale ={2.21} position={[-1.3,-1.3,39.1]} rotation={[-0.22,12.45, 0.06]} />
    <ambientLight />
 
    <OrbitControls />
