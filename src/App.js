38597da8fd718fad947867876b4b23f673e9880e
Ajnur <radovic.ajnur96@gmail.com>
2023-01-16 17:40:45 +0100

    adding loder upon loading model

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React,{useEffect, useState} from 'react'
 import { Canvas,  } from '@react-three/fiber'
 import {  OrbitControls, Text,  } from '@react-three/drei'
 import {Setup} from './Setup'
@@ -6,13 +6,34 @@ import { Suspense } from 'react';
 
 function App() {
 
-  const Loading = () => <Text>Loading...</Text>;
+ 
+
+  const [counter, setCounter] = useState(10);
+  const [stopCounter, setStopCounter] = useState(false);
+
+  useEffect(() => {
+    if (!stopCounter && counter < 100) {
+      setTimeout(() => {
+        setCounter(counter + 1);
+      }, 10);
+    } else {
+      setStopCounter(true);
+    }
+  }, [counter, stopCounter]);
+
+  const Loading = () => <Text>Loading3D Model. Please wait {counter}
+    </Text>
+    ;
+
+
+
   
   return (
     <div style={{ width: "100vw", height: "100vh", background:'black' }}>   
     
     <Canvas camera={{ position: [0, 25, 70], fov: 55, near: 1, far: 20000 }} dpr={[1,2]} > 
     <Suspense fallback={<Loading/>}>
+  
     <Setup receiveShadow castShadow scale ={2.21} position={[-1.3,-1.3,39.1]} rotation={[0,0,0]} />
     </Suspense>   
    <ambientLight color={'white'}/>
